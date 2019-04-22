bb3b6d5158f32f98631053f3e0f1a209a3d2314d
zhaowenshuo <guihuoliuying@163.com>
2019-04-22 23:53:00 +0800

    the thinking of module

---
+++ src/main/java/com/stars/core/module/package-info.java
@@ -0,0 +1,14 @@
+package com.stars.core.module;
+
+/**
+ * 1. life cycle of a player, a service, a module, or a component
+ * 2. packet handler
+ * 3. event handler
+ * 4. a interface to other module, service, component
+ * 5. config data(load, init, reload), more independent
+ * 6. extension of other module(e.g. condition, item function, target)
+ * ----
+ * A. the core module:
+ * 1) the base module system
+ * 2) the
+ */
